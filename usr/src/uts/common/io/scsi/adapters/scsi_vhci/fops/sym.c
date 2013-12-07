1d7afe2802a4f195ad668029ccd61a545f33954a
Saso Kiselkov <saso.kiselkov@dey-sys.com>
2013-12-07 01:47:35 +0000

    Reworking and expansion of f_sym device autodetection.

--- usr/src/uts/common/io/scsi/adapters/scsi_vhci/fops/sym.c
+++ usr/src/uts/common/io/scsi/adapters/scsi_vhci/fops/sym.c
@@ -21,6 +21,7 @@
 
 /*
  * Copyright (c) 2001, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.
  */
 
 /*
@@ -43,16 +44,58 @@ char *symmetric_dev_table[] = {
 /*	"                  111111" */
 /*	"012345670123456789012345" */
 /*	"|-VID--||-----PID------|" */
+/*	see prefix_pattern_match for format description */
 				/* disks */
-	"IBM     DDYFT",
+	"HITACHI HUC######@@S###",	/* Ultrastar (SAS) C7K1000, C10K1200 */
+					/*   C10K900, C10K600, C15K147 */
+	"HITACHI HUS######@@F###",	/* Ultrastar (FC) 15K600 */
+	"HITACHI HUS######@@S###",	/* Ultrastar (SAS) He6, 7K4000, */
+					/*   7K3000, 15K600 */
+	"HITACHI HUSM@####ASS###",	/* Ultrastar SSD800MH, SSD800MM */
+					/*   SSD1000MR, SSD400M */
+	"HITACHI HUSS@####BSS###",	/* Ultrastar SSD400S.B */
+	"HP      DF",			/* HP Enterprise HDDs */
+	"HP      DG",			/* HP Enterprise HDDs */
+	"HP      DH",			/* HP Enterprise HDDs */
+	"HP      EF",			/* HP Enterprise HDDs */
+	"HP      EG",			/* HP Enterprise HDDs */
+	"HP      EH",			/* HP Enterprise HDDs */
+	"HP      EO",			/* HP Enterprise HDDs */
+	"HP      MB",			/* HP Enterprise HDDs */
+	"HP      MM",			/* HP Enterprise HDDs */
+	"HP      ST",			/* HP Enterprise HDDs */
+	"IBM     DDYFT",		/* IBM FC HDDs */
 	"IBM     IC",
-	"SEAGATE ST",
+	"OCZ     TALOS",		/* Talos SSD */
+	"Pliant  LS",			/* Pliant SAS SSDs */
+	"Pliant  LB###",		/* Pliant SAS SSDs */
+	"SEAGATE ST",			/* Seagate Enterprise HDDs/SSDs */
+	"STEC    Z16",			/* ZeusIOPS */
+	"STEC    Z4",			/* ZeusIOPS */
+	"STEC    Zeus",			/* ZeusRAM SAS SSD */
+	"STEC    S###",			/* s800 Series SAS SSD */
+	"TOSHIBA AL13SE@###",		/* AL13SEBxxx, AL13SELxxx */
+	"TOSHIBA MB@###?@@",		/* MBA3xxxFD, MBA3xxxNC, MBA3xxxNP, */
+					/*   MBA3xxxRC, MBB2xxxRC, MBD2xxxRC, */
+					/*   MBE2xxxRC, MBF2xxLRC, MBF2xxxRC */
+	"TOSHIBA MG03@CA###",		/* MG03ACAxxx, MG03SCAxxx */
+	"TOSHIBA MK##0#GRR@",		/* MKxx01GRRB, MKxx01GRRR */
+	"TOSHIBA MK##0#T@KB",		/* MKxx01TRKB, MKxx02TSKB */
+	"TOSHIBA MK##0#GRZB",		/* MKxx01GRZB */
+	"TOSHIBA PX0#S@@###",		/* PX02SMxxxx, PX02SSFxxx, PX03SNxxxx */
+	"WD      WD####@KHG",		/* WD Xe */
+	"WD      WD####FYYG",		/* WD Re */
 				/* enclosures */
 	"SUN     SENA",			/* SES device */
 	"SUN     SESS01",		/* VICOM SVE box */
 	"SUNW    SUNWGS",		/* Daktari enclosure */
 				/* arrays */
+	"3PARdataVV",			/* 3PAR storage */
+	"COMPELNTCompellent Vol",	/* Compellent storage */
 	"HITACHI OPEN",			/* Hitachi storage */
+	"NETAPP  LUN",			/* NetApp storage */
+	"HP      OPEN",			/* HP storage */
+	"HP      HSV2",			/* HP StorageWorks */
 	"SUN     PSX1000",		/* Pirus Matterhorn */
 	"SUN     SE6920",		/* Pirus */
 	"SUN     SE6940",		/* DSP - Nauset */
@@ -63,46 +106,62 @@ char *symmetric_dev_table[] = {
 	"SUN     StorageTek NAS",	/* StorageTek NAS */
 	"SUN     MRA300_R",		/* Shamrock - Controller */
 	"SUN     MRA300_E",		/* Shamrock - Expansion */
-	"STEC    ZeusIOPs",		/* Zeus SAS SSD */
-
+			/* special */
+	"?????????????????SUN??G",	/* Generic Sun supported disks */
+	"?????????????????SUN??T",	/* Generic Sun supported disks */
+	"?????????????????SUN???G",	/* Generic Sun supported disks */
+	"?????????????????SUN???T",	/* Generic Sun supported disks */
+	"ATA     ",			/* Generic ATA device */
 	NULL
 };
 
 /* Failover module plumbing. */
 SCSI_FAILOVER_OP(SFO_NAME_SYM, symmetric);
 
+/*
+ * Perform a match of a pattern on a string. The match is performed from
+ * the start of the string and needn't match its entire length (i.e. it's
+ * a prefix-match), though any direct conflict with the pattern isn't
+ * allowed. The pattern can contain the following special characters:
+ *	? - any character
+ *	@ - any ASCII letter (case insensitive)
+ *	# - any ASCII digit
+ */
+static boolean_t
+prefix_pattern_match(const char *pat, const char *str)
+{
+	for (; *pat && *str; pat++, str++) {
+		switch (*pat) {
+		case '?':	/* any character */
+			break;
+		case '@':	/* A-Za-z */
+			if ((*str < 'a' || *str > 'z') &&
+			    (*str < 'A' || *str > 'Z'))
+				return (B_FALSE);
+			break;
+		case '#':	/* 0-9 */
+			if (*str < '0' || *str > '9')
+				return (B_FALSE);
+			break;
+		default:	/* exact match */
+			if (*pat != *pat)
+				return (B_FALSE);
+			break;
+		}
+	}
+	return (*pat == 0);
+}
+
 /* ARGSUSED */
 static int
 symmetric_device_probe(struct scsi_device *sd, struct scsi_inquiry *stdinq,
-void **ctpriv)
+    void **ctpriv)
 {
 	char	**dt;
-
 	VHCI_DEBUG(6, (CE_NOTE, NULL, "!inq str: %s\n", stdinq->inq_vid));
 	for (dt = symmetric_dev_table; *dt; dt++)
-		if (strncmp(stdinq->inq_vid, *dt, strlen(*dt)) == 0)
+		if (prefix_pattern_match (*dt, stdinq->inq_vid))
 			return (SFO_DEVICE_PROBE_VHCI);
-
-	/*
-	 * No match, check for generic Sun supported disks:
-	 *
-	 *	"|-VID--||-----PID------|"
-	 *	"012345670123456789012345"
-	 *	".................SUN..G."
-	 *	".................SUN..T."
-	 *	".................SUN...G"
-	 *	".................SUN...T"
-	 */
-	if (bcmp(&stdinq->inq_pid[9], "SUN", 3) == 0) {
-		if ((stdinq->inq_pid[14] == 'G' || stdinq->inq_pid[15] == 'G' ||
-		    stdinq->inq_pid[14] == 'T' || stdinq->inq_pid[15] == 'T') &&
-		    (stdinq->inq_dtype == DTYPE_DIRECT)) {
-			return (SFO_DEVICE_PROBE_VHCI);
-		}
-	}
-	if (bcmp(&stdinq->inq_vid[0], "ATA     ", 8) == 0) {
-		return (SFO_DEVICE_PROBE_VHCI);
-	}
 	return (SFO_DEVICE_PROBE_PHCI);
 }
 
